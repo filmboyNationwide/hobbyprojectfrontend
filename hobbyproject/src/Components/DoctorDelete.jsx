b74f070a18b05db7921ef1669679667ed4063625
filmboyJames <filmboynationwide@gmail.com>
2022-10-25 15:28:55 +0100

    Hobby Project Day 2 Delete Component

--- hobbyproject/src/Components/DoctorDelete.jsx
+++ hobbyproject/src/Components/DoctorDelete.jsx
@@ -2,25 +2,19 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 
-function DoctorDelete() {
-  const [newDoctor, setNewDoctor] = useState([]);
-  
-  
+function DoctorDelete() {  
 
   useEffect(()=>{
     const deleteDoctor = async () => {
       try {
         const grab = await axios.patch(`http://localhost:1688/doctors/removeAllDoctors)`);
         console.log('RESPONSE: ', grab);
-        setNewDoctor(grab);
       } catch (err) {
         console.log(err)
       }
     };
     deleteDoctor();
   }, [])
-  
-  console.log(newDoctor)
 
   return <p>All Doctors Deleted</p>>
 }
